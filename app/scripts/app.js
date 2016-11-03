f014bdaecf78aa76ce257ce532e6ec4a7d5f7ad8
leonine90 <leonine90@gmail.com>
2016-11-03 10:32:22 +0330

    install an angular project using Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 100bestsolosApp
+ * @description
+ * # 100bestsolosApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('100bestsolosApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 100bestsolosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 100bestsolosApp
+ */
+angular.module('100bestsolosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
