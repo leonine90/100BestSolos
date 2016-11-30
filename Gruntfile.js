28ceaaaefaccfbb45246b8ec2176ad89bd5f155e
leonine90 <leonine90@gmail.com>
2016-11-30 18:10:59 +0330

    Modify preload of audios

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,8 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '0.0.0.0',
+        hostname: '127.0.0.1',
+		base: 'public',
         livereload: 35729
       },
       livereload: {
