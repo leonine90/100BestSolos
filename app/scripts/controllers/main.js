58d2c73773c72957a15057b64e95381bc2170e7e
leonine90 <leonine90@gmail.com>
2016-12-06 20:10:15 +0330

    Add pause function for other solos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,12 +5,17 @@ angular.module('100bestsolosApp')
   .controller('SolosCtrl', ['$scope','solosFactory', function ($scope,solosFactory) {
       
       $scope.solos = solosFactory.getSolos();
-	  $scope.number = ["1","2","3","4","5"];
+      $scope.playing = 0;
 	  $scope.playSolo = function(i){
+		  	for(var n=1;n<=100;n++){
+				document.getElementById(n).pause();
+			};
 			document.getElementById(i).play();
+			$scope.playing = i;
 	  };
       $scope.pauseSolo = function(i){
 			document.getElementById(i).pause();
+			$scope.playing = 0;
       };
 	  
       
